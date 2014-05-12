6b3226d2ecddc59f3363a5cfe71f084ab145b970
Michael Braun <braunm@smu.edu>
2014-05-12 17:02:19 -0500

    Converted documentation to Roxygen2

---
+++ R/logit.R
@@ -0,0 +1,53 @@
+
+#' @title Logit (log odds) of a probability
+#' @description Returns the logit of x.
+#' @param p a scalar, vector or matrix, where all elements are between 0 and 1
+#' @return result = log(p/(1-p))
+#' @export
+logit <- function(p) {
+
+  if (any(p<=0) || any(p>=1)) {
+    stop (" in function logit:  all elements must be in open interval (0,1)")
+  }
+  
+  res <- log(p) - log(1-p)
+  return(res)
+  
+}
+
+#' @title Inverse logit
+#' @description Returns the inverse logit of elements in x.
+#' @param x a scalar, vector or matrix
+#' @return result = exp(x) / (1+exp(x))
+#' @export
+inv.logit <- function(x) {
+  
+  w.max <- x>=log(.Machine$double.xmax)
+  res <- exp(x - log1p(exp(x)))
+  res[w.max] <- 1
+  return(res)
+
+}
+
+#' @title Log Inverse logit
+#' @description Returns the natural log of the inverse logit of elements in x.
+#' @param x a scalar, vector or matrix
+#' @return result = log(exp(x) / (1+exp(x)))
+#' @details Intended to be a numerically stable alternative to just doing
+#'   log(inv.logit(x)).  Should be less sensitive to overflow and underflow with
+#'   very large or very small x.
+#' @export
+log_inv.logit <- function(x) {
+
+  w.max <- x>=log(.Machine$double.xmax)
+  w.min <- x<=log(.Machine$double.xmin)
+  ww <- !(w.min | w.max)
+  
+  res <- x
+  res[ww] <- x - log1p(exp(x))
+  res[w.max] <- 0
+  return(res)
+  
+}
+
+
---
+++ R/trustOptim.R
@@ -0,0 +1,21 @@
+#' @title Trust-region optimization
+#' 
+#' @description Nonlinear optimizers using trust regions, with methods
+#'   optimized for sparse Hessians.
+#' 
+#' @details
+#' Trust region algorithm for nonlinear optimization. In addition to
+#' being more stable and robust than optim, this package includes 
+#' methods that are scalable and efficient (in terms
+#' of both speed and memory usage) when the Hessian is sparse.
+#' 
+#' @references
+#' Nocedal, Jorge, and Stephen J Wright. 2006. Numerical Optimization. Second edition. Springer.
+#' 
+#' Steihaug, Trond. 1983. The Conjugate Gradient Method and Trust
+#' Regions in Large Scale Optimization. SIAM Journal on Numerical
+#' Analysis 20 (3): 626–637.
+#'
+#' @docType package
+#' @name trustOptim
+NULL 
