7316f30f3a05aadd73ee63b179d3bb48c496a7d0
Michael Braun <braunm@smu.edu>
2015-01-09 19:43:56 -0600

    new vignette that does not depend on sparseHessianFD, but uses the same example model.

---
+++ data-raw/binary.R
@@ -0,0 +1,29 @@
+if (!require(mvtnorm)) {
+    stop("package mvtnorm required")
+}
+if (!require(devtools)) {
+    stop("package devtools required")
+}
+
+set.seed(123)
+
+N <- 200
+k <- 2
+T <- 100
+
+x.mean <- rep(0,k)
+x.cov <- diag(k)
+mu <- rnorm(k, 0, 1)
+Omega <- diag(k)
+
+X <- t(rmvnorm(N, mean=x.mean, sigma=x.cov)) ## k x N
+B <- t(rmvnorm(N, mean=mu, sigma=Omega)) ## k x N
+XB <- colSums(X * B)
+log.p <- XB - log1p(exp(XB))
+Y <- sapply(log.p, function(q) return(rbinom(1,T,exp(q))))
+
+binary <- list(Y=Y, X=X, T=T)
+
+devtools::use_data(binary, overwrite=TRUE)
+ 
+
---
+++ inst/doc/trustOptim-jss.R
@@ -0,0 +1,2 @@
+### R code from vignette source 'trustOptim-jss.Rnw'
+
