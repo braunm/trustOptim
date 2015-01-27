fc37f2eac90a9c6b4a604e5041f8b633ab4c6b25
Michael Braun <braunm@smu.edu>
2015-01-27 12:29:38 -0600

    updated documentation and namespace

--- inst/include/common_R.hpp
+++ inst/include/common_R.hpp
@@ -2,7 +2,7 @@
 // common_R.hpp -- this file is part of trustOptim, a nonlinear optimization package
 // for the R statistical programming platform.
 //
-// Copyright (C) 2013 Michael Braun
+// Copyright (C) 2013-2015 Michael Braun
 //
 // This Source Code Form is subject to the license terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
