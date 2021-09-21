9f22c5c811372d4817982c91cccaa645e74681c5
Michael Braun <braunm@smu.edu>
2021-09-20 23:10:23 -0500

    set up pkgdown site

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // sparseTR
 List sparseTR(NumericVector start, Function fn, Function gr, Function hs, const List control);
 RcppExport SEXP _trustOptim_sparseTR(SEXP startSEXP, SEXP fnSEXP, SEXP grSEXP, SEXP hsSEXP, SEXP controlSEXP) {
